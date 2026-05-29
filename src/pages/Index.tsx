3e0527e713fbaff690b31b55e99f98c302db8dbb
JesseQin123 <jesseqin.office@gmail.com>
2026-05-29 17:52:17 -0400

    Redesign homepage around solo unicorn brand

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,25 +1,7 @@
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import CredibilitySection from "@/components/CredibilitySection";
-import ServicesSection from "@/components/ServicesSection";
-import PricingSection from "@/components/PricingSection";
-import ContactSection from "@/components/ContactSection";
-import Footer from "@/components/Footer";
+import SoloUnicornHomepage from "@/components/SoloUnicornHomepage";
 
 const Index = () => {
-  return (
-    <div className="min-h-screen bg-background">
-      <Navbar />
-      <main>
-        <HeroSection />
-        <CredibilitySection />
-        <ServicesSection />
-        <PricingSection />
-        <ContactSection />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <SoloUnicornHomepage />;
 };
 
 export default Index;
